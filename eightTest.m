d949bdf3fba9e9911ef3830032538581a5b9f292
emikob <emikoelizabethbell@gmail.com>
2020-12-07 23:31:45 +0000

    patterns output

--- eightTest.m
+++ eightTest.m
@@ -1,10 +1,12 @@
 function eightTest
 
-binaryElement = zeros(600, 600);
+element = zeros(600, 600); %Creating array with zeros
 
-    for i = 1:8
-    unit = GenerateThreeByThreeElement(i);
-    binaryElement = repmat(unit, 200, 200);
-    imwrite(binaryElement, strcat('Pattern', num2str(i), '.png'))
+    for i = 1:8 %Repeating for loop 8 times
+    unit = GenerateThreeByThreeElement(i); %Calling GenerateThreeByThreeElement function to create an element for each iteration from 1-8
+    element = repmat(unit, 200, 200); %Repeating the 3x3 element by 200 rows and columns to produce a 600x600 image
+    imwrite(element, strcat('Pattern', num2str(i), '.png')) %Creating .png  
+    figure(i)
+    imshow(['Pattern' num2str(i) '.png'])
     end
 end
