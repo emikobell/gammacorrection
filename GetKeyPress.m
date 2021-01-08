5ad3dc53a16c11282e52091be0e6d98c9cf0ad90
Emiko Bell <64177820+emikob@users.noreply.github.com>
2021-01-08 18:29:34 +0000

    Updated Explanations

--- GetKeyPress.m
+++ GetKeyPress.m
@@ -8,7 +8,7 @@ character = getkey('non-ascii'); % getkey function takes one input from the keyb
 
 accepted = ['h' 'b' 'j' 'n' 'm' 'q'];
 
-    while isempty(character) | ~ismember(character, accepted) | length(character) > 1 % The last argument avoids modifier keys (e.g. enter, space). No guarantee if the array is scalar, so the | argument is used.
+    while isempty(character) | ~ismember(character, accepted) | length(character) > 1 % The first argument avoids NaN characters when non-ASCII characters are used, the second checks for approved letters, and the last avoids modifier keys (e.g. enter, space). No guarantee if the array is scalar, so the | argument is used.
           beep; % Play error noise when the input is invalid
           character = getkey('non-ascii'); % Reassigns character with new input
     end
