5af7363e17f74c178984648aeba93e90b215a3a8
Emiko Bell <64177820+emikob@users.noreply.github.com>
2021-01-15 17:32:43 +0000

    Final final?

--- RunGreyMatching.m
+++ RunGreyMatching.m
@@ -8,8 +8,7 @@ function RunGreyMatching(participantID)
 oldLevel = Screen('Preference', 'Verbosity', 1); % Store the old PTB message visibility option, then hiding the visibility
 PerceptualGamma = zeros(8, 4); % Create a matrix for the PerceptualGamma data
 PerceptualGamma(:, 1) = 1:8; % Fill the first column of the table with the pattern numbers
-rng(participantID); % Initialise random number generator with the participantID as the seed
-patternNumber = [1:8] % Create an array of pattern numbers
+patternNumber = [1:8]; % Create an array of pattern numbers
 
 window = OpenPTBWindow; %Open a black PTB window and output the window pointer for future scripts
 
@@ -24,13 +23,13 @@ window = OpenPTBWindow; %Open a black PTB window and output the window pointer f
         
     end
 
-DrawFormattedText(window, 'Thank you, this is the end of the experiment. \n The Gamma Data CSV has been saved to your working directory.', 'center', 200, white, [], [], [], [2]); % Draw completion text 200 pixels down from the top of the screen
+DrawFormattedText(window, 'Thank you, this is the end of the experiment. \n The Gamma Data CSV has been saved to your working directory. \n Press any key to exit', 'center', 200, [255 255 255], [], [], [], [2]); % Draw completion text 200 pixels down from the top of the screen
 Screen(window, 'Flip'); % Show drawn text on the screen
 KbWait; % Wait for input
 
 PerceptualGamma = array2table(PerceptualGamma); % Convert data from matrix to a table for labels
 PerceptualGamma.Properties.VariableNames = {'Pattern','Repeat 1','Repeat 2','Repeat 3'}; % Add header with labels
-writetable(PerceptualGamma, ['PerceptualGamma_' int2str(participantID) '.csv']); % Save table as a CSV with the participant ID
+writetable(PerceptualGamma, ['PerceptualGamma_' participantID '.csv']); % Save table as a CSV with the participant ID
 
 Screen('CloseAll'); % Close PTB windows
 Screen('Preference', 'Verbosity', oldLevel); % Return PTB messages
